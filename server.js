bc6c353f170c8639560f3b6116b1b201f017c011
Ryan Rowland <rrowland@twilio.com>
2015-12-14 14:05:39 -0800

    Update to use new token format

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ var TokenProvider = require('./lib/tokenprovider');
 var app = new express();
 var tokenProvider = new TokenProvider(credentials);
 
+if (credentials.authToken) {
+  console.warn('WARNING: The "authToken" field is deprecated. Please use "signingKeySecret".');
+}
+
+if (credentials.instanceSid) {
+  console.warn('WARNING: The "instanceSid" field is deprecated. Please use "serviceSid".');
+}
+
 app.get('/getToken', function(req, res) {
   var identity = req.query && req.query.identity;
   var endpointId = req.query && req.query.endpointId;
